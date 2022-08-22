a2f6e18a56eda6255c6fa16ab0684c459289c062
xchavezx <ccordoba@biometricpro.io>
2022-08-22 16:05:18 -0300

    dsafs

--- resources/views/customers/table.blade.php
+++ resources/views/customers/table.blade.php
@@ -1,29 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="panel-heading">Simple table</div>
+    <div class="panel-heading">Row details</div>
     <div class="panel-body">
-        <table class="table">
+        <table class="table table-bordered" id="customers-table">
             <thead>
-            <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Email</th>
-                <th>Created at</th>
-                <th>Updated at</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($customers as $customer)
                 <tr>
-                    <td>{{ $customer->first_name }}</td>
-                    <td>{{ $customer->last_name }}</td>
-                    <td>{{ $customer->email }}</td>
-                    <td>{{ $customer->created_at }}</td>
-                    <td>{{ $customer->updated_at }}</td>
+                    <th>Id</th>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                    <th>Created At</th>
+                    <th></th>
                 </tr>
-            @endforeach
-            </tbody>
+            </thead>
         </table>
     </div>
+@endsection
+
+@section('javascript')
+
+    <script>
+          $('#customers-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: '{{ route('api.row_attributes') }}',
+            columns: [
+              { data: 'id', name: 'id' },
+              { data: 'first_name', name: 'first_name' },
+              { data: 'last_name', name: 'last_name' },
+              { data: 'email', name: 'email' },
+              { data: 'created_at', name: 'created_at' },
+              { data: 'action', name: 'action', orderable: false, searchable: false }
+            ]
+          });
+    </script>
 @endsection
