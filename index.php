4ff1e5fe77ca37d5a8308b562ae6451203defe7d
xchavezx <ccordoba@biometricpro.io>
2022-08-22 19:21:41 -0300

    dsafs

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
-
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="jquery.js"></script>
 <table id="table_id" class="display">
     <thead>
         <tr>
